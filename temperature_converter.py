04968ffad1f9ea15893c383bd58496949d19bb57
Truman2557 <144353279+Truman2557@users.noreply.github.com>
2023-11-28 15:10:32 +0000

    added f to k and k to f

--- temperature_converter.py
+++ temperature_converter.py
@@ -14,6 +14,16 @@ def f_to_c(temp_f):
     converted_f = (temp_f - 32) * (5/9)
     return converted_f
 
+#function that converts K to F
+def k_to_f(temp_k):
+    converted_k = (temp_k - 273.15) * (9/5) + 32
+    return converted_k
+
+#function that converts F to K
+def f_to_k(temp_fk):
+    converted_fk = (temp_fk - 32) * (5/9) + 273.15 
+    return converted_fk
+
 #main function that uses conditionals to decide which convert function to select
 def main():
     if(unit == "C"):
@@ -22,8 +32,14 @@ def main():
     elif(unit == "F"):
         fahrenheit_to_celsius = f_to_c(value)
         return fahrenheit_to_celsius
+    elif(unit == "K"):
+        kelvin_to_fahrenheit = k_to_f(value)
+        return kelvin_to_fahrenheit
+    elif(unit == "FK"):
+        fahrenheit_to_kelvin = f_to_k(value)
+        return fahrenheit_to_kelvin
     else:
-        warning = "Please enter C or F to specify the unit: "
+        warning = "Please enter C, F, K, or FK to specify the unit: "
         return warning
 
 
